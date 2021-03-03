2b8bdc36bac1465353aa5fcfa73d7d7db2cf4c04
CarolKuch <kar.kucharska@gmail.com>
2021-03-03 17:58:05 +0100

    Img carousel added to createHeader.js

--- src/common/createHeader.js
+++ src/common/createHeader.js
@@ -2,9 +2,50 @@ import $ from 'jquery';
 
 export const createHeader = () => {
     const header = $(`
-        <header class="main-header">
-            <h1>SPA dla nerdów</h1>
-        </header>
+    <div id="header-carousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#header-carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#header-carousel" data-slide-to="1"></li>
+            <li data-target="#header-carousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="https://images.unsplash.com/photo-1565297032488-90722f09db62?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80" alt="First slide">
+                <div class="carousel-caption d-none d-md-block caption1">
+                    <header class="main-header">
+                        <h1>SPA dla nerdów</h1>
+                        <p>Jedyne, a więc najlepsze SPA wyłącznie dla programistów!</p>
+                    </header>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="https://images.pexels.com/photos/2506988/pexels-photo-2506988.jpeg?cs=srgb&dl=pexels-quark-studio-2506988.jpg&fm=jpg" alt="Second slide">
+                <div class="carousel-caption d-none d-md-block caption2">
+                    <header class="main-header">
+                        <h1>SPA dla nerdów</h1>
+                        <p>Jedyne, a więc najlepsze SPA wyłącznie dla programistów!</p>
+                    </header>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="https://cdn.pixabay.com/photo/2017/02/27/15/12/rice-2103481_1280.jpg" alt="Third slide">
+                <div class="carousel-caption d-none d-md-block caption3">
+                    <header class="main-header">
+                        <h1>SPA dla nerdów</h1>
+                        <p>Jedyne, a więc najlepsze SPA wyłącznie dla programistów!</p>
+                    </header>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#header-carousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#header-carousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+  </div>
     `);
     return header;
 }
--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -4,7 +4,7 @@ import * as views from '../views';
 export const navigation = () => {
     const fragment = $(document.createDocumentFragment());
     const nav = $(`
-    <nav class="top-nav navbar navbar-expand-md navbar-dark bg-dark">
+    <nav class="top-nav navbar navbar-expand-md navbar-dark bg-transparent">
         <a class="navbar-brand" href="#">SPA dla nerdów</a> 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -14,7 +14,11 @@ export const navigation = () => {
     const buttons = Object
                         .keys(views)
                         .map(viewName => {
-                            const button = $(`<li class="nav-button">${viewName.toUpperCase()}</li>`);
+                            const button = $(`
+                                <div class="top-nav-container">
+                                    <li class="nav-button">${viewName.toUpperCase()}</li>
+                                </div>
+                            `);
 
                             button.on('click', () => {
                                 const customEvent = new CustomEvent('navigation', {
