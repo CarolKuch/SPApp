64db23781bd4056c1d34d056c8a71a217c8fc7c6
CarolKuch <kar.kucharska@gmail.com>
2021-03-17 11:07:33 +0100

    Change total price when quantity changed

--- src/common/basketBehavior.js
+++ src/common/basketBehavior.js
@@ -2,24 +2,14 @@ import $ from 'jquery';
 let counter = 0;
 
 export let basketCounter = (info) => {
-    if (info != "nav") {
-        counter++;
-    };
+    if (info != "nav") counter++;
     return counter;
 }
 
 export let basketBehavior = () => {
-
-    var removeBasketItemButtons = $(document).find('.btn-danger');
-    for(let i = 0; i < removeBasketItemButtons.length; i++){
-        let button = removeBasketItemButtons[i];
-        $(button).on('click', (e) => {
-            let buttonClicked = e.target;
-            buttonClicked.parentElement.parentElement.remove();
-            updateBasketTotal();
-        })
-    }
-
+    let removeBasketItemButtons = $(document).find('.btn-danger');
+    let quantityInputs = $(document).find('.basket-quantity');
+    
     let updateBasketTotal = () => {
         let basketRows = $(document).find('.basket-row');
         let total = 0;
@@ -29,8 +19,33 @@ export let basketBehavior = () => {
             let quantityContainer = $(basketRow).find('.basket-quantity')[0];
             let price = priceContainer.innerText.replace('zł', '');
             let quantity = quantityContainer.value;
-            total =+ quantity * price;
+            total += quantity * price;
         }
-        $(basketRows).find('.basket-total-price').prepend(total);
+        total = Math.round(total*100)/100;
+        $(basketRows).find('.basket-total-price').empty().append(total+" zł");
+    }
+
+    updateBasketTotal();
+
+    let removeBasketItem = (e) => {
+        let buttonClicked = e.target;
+        buttonClicked.parentElement.parentElement.remove();
+        updateBasketTotal();
+    }
+
+    let quantityChanged = (e) => {
+        let input = e.target;
+        if( isNaN(input.value) || input.value <= 0 ) input.value = 1;
+        updateBasketTotal();
+    }
+
+    for(let i = 0; i < quantityInputs.length; i++){
+        let input = quantityInputs[i];
+        $(input).on('change', quantityChanged);
+    }
+
+    for(let i = 0; i < removeBasketItemButtons.length; i++){
+        let button = removeBasketItemButtons[i];
+        $(button).on('click', removeBasketItem);
     }
 }
